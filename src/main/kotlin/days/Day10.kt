84537747087980b87e483634b5557396ec27da4f
Shykial <szymon.jeziorski@outlook.com>
2024-12-10 07:49:36 +0100

    Day 10 done

---
+++ src/main/kotlin/days/Day10.kt
@@ -0,0 +1,54 @@
+package days
+
+import utils.Coordinates
+import utils.Move
+import utils.Point
+import utils.getOrNull
+import utils.plus
+import utils.pointsSequence
+import utils.readInputLines
+
+object Day10 {
+    fun part1(input: List<String>): Int = parseInput(input).countTrailheadsScore()
+
+    fun part2(input: List<String>): Int = parseInput(input).countHikingPathsScore()
+
+    data class TopographicMap(val values: List<List<Int>>) {
+        private val starts = values.pointsSequence()
+            .filter { it.value == 0 }
+            .toList()
+
+        fun countTrailheadsScore() = starts.sumOf { it.countReachableTrailheads(mutableSetOf()) }
+
+        fun countHikingPathsScore() = starts.sumOf { it.countUniqueHikingPaths() }
+
+        private fun Point<Int>.countReachableTrailheads(seenHeads: MutableSet<Coordinates>): Int {
+            if (value == 9) return if (seenHeads.add(coordinates)) 1 else 0
+            return hikingSequence().sumOf { Point(value + 1, it).countReachableTrailheads(seenHeads) }
+        }
+
+        private fun Point<Int>.countUniqueHikingPaths(): Int {
+            if (value == 9) return 1
+            return hikingSequence().sumOf { Point(value + 1, it).countUniqueHikingPaths() }
+        }
+
+        private fun Point<Int>.hikingSequence() = allowedMoves.asSequence()
+            .map { coordinates + it }
+            .filter { values.getOrNull(it) == value + 1 }
+    }
+
+    private val allowedMoves = listOf(
+        Move(-1, 0),
+        Move(1, 0),
+        Move(0, -1),
+        Move(0, 1),
+    )
+
+    private fun parseInput(input: List<String>) = TopographicMap(input.map { it.map(Char::digitToInt) })
+}
+
+fun main() {
+    val input = readInputLines("Day10")
+    println("part1: ${Day10.part1(input)}")
+    println("part2: ${Day10.part2(input)}")
+}
--- src/main/kotlin/utils/GridUtils.kt
+++ src/main/kotlin/utils/GridUtils.kt
@@ -12,6 +12,8 @@ operator fun Coordinates.minus(other: Coordinates) = Move(yShift = y - other.y, 
 
 fun List<CharSequence>.getOrNull(coordinates: Coordinates): Char? = getOrNull(coordinates.y)?.getOrNull(coordinates.x)
 
+fun <T> List<List<T>>.getOrNull(coordinates: Coordinates): T? = getOrNull(coordinates.y)?.getOrNull(coordinates.x)
+
 operator fun List<CharSequence>.contains(coordinates: Coordinates) = getOrNull(coordinates) != null
 
 data class Point<T>(val value: T, val coordinates: Coordinates)
@@ -25,7 +27,13 @@ infix fun Move.turn(turn: Turn) = when (turn) {
 
 data class MovingPoint(val coordinates: Coordinates, val move: Move)
 
+@JvmName("stringsPointsSequence")
 fun List<String>.pointsSequence(): Sequence<Point<Char>> =
     asSequence().flatMapIndexed { y, string ->
         string.mapIndexed { x, char -> Point(char, Coordinates(y, x)) }
+    }
+
+fun <T> List<List<T>>.pointsSequence(): Sequence<Point<T>> =
+    asSequence().flatMapIndexed { y, row ->
+        row.mapIndexed { x, element -> Point(element, Coordinates(y, x)) }
     }
